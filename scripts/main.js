b0dcc4f92770903323a95a4dab05f2edebecc5a4
Rob Cowley <robcowley06@hotmail.co.uk>
2017-10-31 14:37:12 +0000

    Grid & lanes now populate

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,23 @@
-let app = {
+var app = {
 	context: null, // audio context instance
-	tempo: 100,
-	stepIntervalMs: null,
-	pos: 1,
-	totalSteps: 16,
-	kit : {}, //empty kit object (gets populated from kits.json on init)
-	sequence : [], //gets populated dynamically on init
+	swingIntervalMs: null,
+	swingIntervalA: null,
+	swingIntervalB: null,
+	tempo: 124, //tempo in BPM
+	swing: true, //specifies whether pattern plays back with swing
+	swingAmount: 69, //percentage
+	current: 0, //current position in sequence
+	seqLength: 16, //number of steps in sequence
+	play: false,
+	loop: true,
+	swing: false,
+	softHitVol: 0.4,
+	hardHitVol: 1.0,
+	kit : {}, // empty kit object (gets populated from kits.json on init)
+	sequence : {
+		kick:  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+		snr:   [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0]
+	},
 
 	createContext: function() {
 		try {
@@ -26,7 +38,7 @@ let app = {
 			if (obj.hasOwnProperty(i)) {
 				// load sound
 				this.loadSoundObj(obj[i]);
-				console.log(i + ' loaded');
+				console.log(`${i} loaded`);
 			}
 		}
 		this.populateGridHtml();
@@ -48,42 +60,109 @@ let app = {
 		request.send();
 	},
 
+	playSequence: function() {
+		for(var drum in app.kit) {
+			var val = app.sequence[drum][app.current]; //1 or zero depending on position in sequence
+			var buff = app.kit[drum].buffer;
+
+			app.highlightGridPosition(drum);
+			app.stepCounter();
+
+			//calls playSnd if sequence value is 1 or 2
+			// if(val == 2) {
+			// 	app.playSnd(buff, app.hardHitVol);
+			// }
+			// else if(val == 1) {
+			// 	app.playSnd(buff, app.softHitVol);
+			// }
+			if(val == 1) {
+				app.playSnd(buff);
+			}
+		}
+		if(!app.swing) {
+			if((++app.current < app.seqLength)) {
+				setTimeout(app.playSequence, app.stepIntervalMs);
+			}
+			else if (app.loop == 1) {
+				setTimeout(app.playSequence, app.stepIntervalMs);
+				app.current = 0;
+			}
+		}
+		else {
+			if((++app.current < app.seqLength)) {
+				if(app.current % 2 == 0) {
+					setTimeout(app.playSequence, app.swingIntervalB);
+				}
+				else {
+					setTimeout(app.playSequence, app.swingIntervalA);
+				}
+			}
+			else if (app.loop == 1) {
+				setTimeout(app.playSequence, app.swingIntervalB);
+				app.current = 0;
+			}
+		}
+	},
+
+	padClick: function(el) {
+		var padNumber = el.innerHTML - 1;
+		var drum = el.getAttribute('data-drum');
+		var sequenceArrLane = app.sequence[drum];
+		$(el).toggleClass('active');
+		if(sequenceArrLane[padNumber] == 1) {
+			sequenceArrLane[padNumber] = 0;
+		}
+		else {
+			sequenceArrLane[padNumber] = 1;
+		}
+	},
+
 	populateGridHtml: function() {
-		const kitObj = this.kit;
-		var grid = document.getElementById("grid");
+		var kitObj = this.kit;
+		var grid = document.getElementById('grid');
 
 		for(var a in kitObj) {
-			var ul = document.createElement("ul");
-			var laneLabel = document.createElement("li");
+			var ul = document.createElement('ul');
+			var laneLabel = document.createElement('li');
 			var laneLabelText = document.createTextNode(a);
 
 			ul.className = 'pads';
 			ul.dataset.lane = a;
 			grid.appendChild(ul);
 
-			var newUl = document.querySelector('[data-lane="' + a + '"]');
+			var newUl = document.querySelector(`[data-lane="${a}"]`);
 
 			laneLabel.className = 'pad-label';
 			laneLabel.appendChild(laneLabelText);
 			newUl.appendChild(laneLabel);
 
 			for(var b = 1; b < 17; b++) {
-				var pad = document.createElement("li");
+				var pad = document.createElement('li');
 				var text = document.createTextNode(b);
 
 				pad.className = 'pad';
 				pad.appendChild(text);
+				pad.addEventListener('click', function() {
+					app.padClick(this);
+				});
 				pad.dataset.drum = a;
 				ul.appendChild(pad);
 			}
 		}
-		$('.pad').on('touchstart', function() {
-			app.padClick(this);
-		});
+		this.populateLanes();
 	},
 
-	padClick: function(el) {
-		$(el).toggleClass('active');
+	populateLanes: function() {
+		for(var drum in app.sequence) {
+			var currentLane = app.sequence[drum];
+			var seqLanePadEls = document.querySelectorAll(`[data-drum="${drum}"]`);
+			var length = currentLane.length;
+			for(var i = 0; i < length; i++) {
+				if(currentLane[i] == 1) {
+					seqLanePadEls[i].className += ' active';
+				}
+			}
+		}
 	},
 
 	init: function() {
@@ -93,10 +172,9 @@ let app = {
 		$.getJSON('json/kits.json', function(json) {
 			app.kit = json;
 			console.log('json file loaded');
-		})
-			.done(function(data) {
-				app.loadSounds(app.kit);
-			});
+		}).done(function(data) {
+			app.loadSounds(app.kit);
+		});
 		this.stepIntervalMs = 60000 / this.tempo / 4;
 	}
 }
